d6c9127b7b304036114cd188d23823b3a8f107f4
Jarvis73 <zjw.math@qq.com>
2021-07-14 23:42:29 +0800

    Optimize memory usage (#11)

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Moving Least Squares (MLS)
 
-**Update: 2020-09-25** No need for so-called inverse transformation. Just transform target pixels to the corresponding source pixels.
+**Update:** 
+
+*   **2021-07-14:**  Optimize memory usage. Now a 2000x2000 image with 64 control points spend about 4.2GB memory. (20GB in the previous version)
+
+*   **2020-09-25:**  No need for so-called inverse transformation. Just transform target pixels to the corresponding source pixels.
 
 ## Introduction
 **Moving least squares** is a method of reconstructing continuous functions from a set of unorganized point samples via the calculation of a weighted least squares measure biased towards the region around the point at which the reconstructed value is requested.
@@ -15,15 +19,11 @@ In computer graphics, the moving least squares method is useful for reconstructi
 ## Preview
 * Toy
 
-![Affine deformation](https://github.com/jarvis73/Moving-Least-Squares/raw/master/images/toy_1_affine.png)
-
-![Similarity deformation](https://github.com/jarvis73/Moving-Least-Squares/raw/master/images/toy_2_similarity.png)
-
-![Rigid deformation](https://github.com/jarvis73/Moving-Least-Squares/raw/master/images/toy_3_rigid.png)
+![Deformation](https://github.com/jarvis73/Moving-Least-Squares/raw/master/images/toy_results.png)
 
 * Monalisa
 
-![Rigid deformation](https://github.com/jarvis73/Moving-Least-Squares/raw/master/images/monalisa_3_rigid.png)
+![Rigid deformation](https://github.com/jarvis73/Moving-Least-Squares/raw/master/images/monalisa_rigid.png)
 
 * Cells
 
@@ -35,5 +35,26 @@ In computer graphics, the moving least squares method is useful for reconstructi
 * `read_tif.py`: TIF file reader
 * `tiff_deformation.py`: Demo program
 
+### Optimize memory usage
+
+*   Here lists some examples of memory usage and running time
+
+| Image Size  | Control Points | Affine         | Similarity     | Rigid          |
+| ----------- | -------------- | -------------- | -------------- | -------------- |
+| 500 x 500   | 16             | 0.57s / 0.15GB | 0.99s / 0.16GB | 0.89s / 0.13GB |
+| 500 x 500   | 64             | 1.6s / 0.34GB  | 3.7s / 0.3GB   | 3.6s / 0.2GB   |
+| 1000 x 1000 | 64             | 7.7s / 1.1GB   | 17s / 0.98GB   | 15s / 0.82GB   |
+| 2000 x 2000 | 64             | 30s / 4.2GB    | 65s / 3.6GB    | 69s / 3.1GB    |
+
+*   Estimate memory usage for large image: (h x w x N x 4 x 2) x 2~2.5
+    *   h, w: image size
+    *   N: number of control points
+    *   4: float32
+    *   2: coordinates (x, y)
+    *   2~2.5: intermediate results
+
+
+
 ## Reference
+
 [1] Schaefer S, Mcphail T, Warren J. Image deformation using moving least squares[C]// ACM SIGGRAPH. ACM, 2006:533-540.
--- img_utils_demo.py
+++ img_utils_demo.py
@@ -4,24 +4,19 @@
 """
 Image deformation using moving least squares
 
-@author: Jarvis ZHANG
+@author: Jian-Wei ZHANG
 @date: 2017/8/8
 @update: 2020/9/25
+@update: 2021/7/14: simplify usage
 """
 
-import os
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from img_utils import mls_affine_deformation, mls_similarity_deformation, mls_rigid_deformation
+from PIL import Image
 
 
-def show_example():
-    img = plt.imread(os.path.join(sys.path[0], "double.jpg"))
-    plt.imshow(img)
-    plt.show()
-
-def demo(fun, name):
+def demo():
     p = np.array([
         [30, 155], [125, 155], [225, 155],
         [100, 235], [160, 235], [85, 295], [180, 293]
@@ -30,61 +25,81 @@ def demo(fun, name):
         [42, 211], [125, 155], [235, 100],
         [80, 235], [140, 235], [85, 295], [180, 295]
     ])
-    image = plt.imread(os.path.join(sys.path[0], "mr_big_ori.jpg"))
-
-    plt.figure(figsize=(8, 3))
-    plt.subplot(131)
-    plt.axis('off')
-    plt.imshow(image)
-    plt.title("Original Image")
     
-    transformed_image = fun(image, p, q, alpha=1, density=1)
-    plt.subplot(132)
-    plt.axis('off')
-    plt.imshow(transformed_image)
-    plt.title("%s Deformation \n Sampling density 1"%name)
-    transformed_image = fun(image, p, q, alpha=1, density=0.7)
-    plt.subplot(133)
-    plt.axis('off')
-    plt.imshow(transformed_image)
-    plt.title("%s Deformation \n Sampling density 0.7"%name)
+    image = np.array(Image.open("images/toy.jpg"))
+    
+    height, width, _ = image.shape
+    gridX = np.arange(width, dtype=np.int16)
+    gridY = np.arange(height, dtype=np.int16)
+    vy, vx = np.meshgrid(gridX, gridY)
+    
+    affine = mls_affine_deformation(vy, vx, p, q, alpha=1)
+    aug1 = np.ones_like(image)
+    aug1[vx, vy] = image[tuple(affine)]
+
+    similar = mls_similarity_deformation(vy, vx, p, q, alpha=1)
+    aug2 = np.ones_like(image)
+    aug2[vx, vy] = image[tuple(similar)]
+
+    rigid = mls_rigid_deformation(vy, vx, p, q, alpha=1)
+    aug3 = np.ones_like(image)
+    aug3[vx, vy] = image[tuple(rigid)]
+
+
+    fig, ax = plt.subplots(1, 4, figsize=(12, 4))
+    ax[0].imshow(image)
+    ax[0].set_title("Original Image")    
+    ax[1].imshow(aug1)
+    ax[1].set_title("Affine Deformation")
+    ax[2].imshow(aug2)
+    ax[2].set_title("Similarity Deformation")
+    ax[3].imshow(aug3)
+    ax[3].set_title("Rigid Deformation")
+
+    for x in ax.flat:
+        x.axis("off")
 
     plt.tight_layout(w_pad=0.1)
     plt.show()
 
-def demo2(fun, name):
-    ''' 
-        Smiled Monalisa  
-    '''
+
+def demo2():
+    """ Smiled Monalisa """
     
-    p = np.array([
+    p = np.array([[0, 0], [0, 517], [798, 0], [798, 517],
         [186, 140], [295, 135], [208, 181], [261, 181], [184, 203], [304, 202], [213, 225], 
         [243, 225], [211, 244], [253, 244], [195, 254], [232, 281], [285, 252]
     ])
-    q = np.array([
+    q = np.array([[0, 0], [0, 517], [798, 0], [798, 517],
         [186, 140], [295, 135], [208, 181], [261, 181], [184, 203], [304, 202], [213, 225], 
         [243, 225], [207, 238], [261, 237], [199, 253], [232, 281], [279, 249]
     ])
-    image = plt.imread(os.path.join(sys.path[0], "monalisa.jpg"))
-    plt.figure(figsize=(8, 8))
-    plt.subplot(121)
-    plt.axis('off')
-    plt.imshow(image)
-    plt.title("Original Image")
-    transformed_image = fun(image, p, q, alpha=1, density=1)
-    plt.subplot(122)
-    plt.axis('off')
-    plt.imshow(transformed_image)
-    plt.title("%s Deformation" % name)
+
+    image = np.array(Image.open("images/monalisa.jpg"))
+
+    height, width, _ = image.shape
+    gridX = np.arange(width, dtype=np.int16)
+    gridY = np.arange(height, dtype=np.int16)
+    vy, vx = np.meshgrid(gridX, gridY)
+
+    rigid = mls_rigid_deformation(vy, vx, p, q, alpha=1)
+    aug = np.ones_like(image)
+    aug[vx, vy] = image[tuple(rigid)]
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+    ax[0].imshow(image)
+    ax[0].set_title("Original Image")
+    ax[1].imshow(aug)
+    ax[1].set_title("Rigid Deformation")
+
+    for x in ax.flat:
+        x.axis("off")
+    
     plt.tight_layout(w_pad=1.0, h_pad=1.0)
     plt.show()
 
 
 if __name__ == "__main__":
-    demo(mls_affine_deformation, "Affine")
-    demo(mls_similarity_deformation, "Similarity")
-    demo(mls_rigid_deformation, "Rigid")
+    # demo()
+    demo2()
 
-    demo2(mls_affine_deformation, "Affine")
-    demo2(mls_similarity_deformation, "Similarity")
-    demo2(mls_rigid_deformation, "Rigid")
